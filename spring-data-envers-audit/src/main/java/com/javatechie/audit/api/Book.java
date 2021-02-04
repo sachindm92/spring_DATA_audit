cbcd0cf2b08540c350a3306a513d157670b549fd
user <user@DESKTOP-KAFKOSO>
2021-02-04 15:51:27 +0530

    This is a spring boot hibernate audit project.

---
+++ spring-data-envers-audit/src/main/java/com/javatechie/audit/api/Book.java
@@ -0,0 +1,42 @@
+package com.javatechie.audit.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Audited
+public class Book {
+@Id
+@GeneratedValue
+    private int id;
+    private String name;
+    private int pages;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPages() {
+		return pages;
+	}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+    
+}
